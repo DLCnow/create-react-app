c73fb70c28ce86a6b904104c1fa45a14068291c0
fxghqc <konxiao@gmail.com>
2018-03-24 16:15:46 +0800

    use react-router

---
+++ packages/react-scripts/template/src/layouts/AdminLayout.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AdminLayout extends Component {
+  render() {
+    return <div>AdminLayout</div>;
+  }
+}
+
+export default AdminLayout;
---
+++ packages/react-scripts/template/src/routes/Home/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <p className="text-center my-4">Hello, React.</p>;
+
+export default Home;
